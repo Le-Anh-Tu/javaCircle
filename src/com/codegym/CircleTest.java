47239f6894e8e10b629187b5e47ae1329e14f945
le tu <leanhtu280295@gmail.com>
2021-08-11 16:51:31 +0700

    đuongan

---
+++ src/com/codegym/CircleTest.java
@@ -0,0 +1,14 @@
+package com.codegym;
+
+import javafx.scene.shape.Circle;
+
+public class CircleTest {
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(5);
+        System.out.println(circle);
+    }
+}
